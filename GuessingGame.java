b0761f2d20ebb3ad7a14db1ed1bc1307fa42b1ac
fentercj <fentercj@mail.uc.edu>
2021-03-29 17:08:34 +0000

    Initial commit

---
+++ GuessingGame.java
@@ -0,0 +1,83 @@
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+
+import java.util.Random;
+
+public class GuessingGame implements ActionListener{
+JTextField userGuess;
+
+JButton guess;
+JButton again;
+
+JLabel enter;
+JLabel highLow;
+JLabel last;
+
+GuessingGame(){
+  Random r = new Random();
+  int random = r.nextInt(100) + 1;
+
+  JFrame frame = new JFrame("Guessing Game");
+  frame.setLayout(new FlowLayout());
+  frame.setSize(240,120);
+
+  userGuess = new JTextField(10);
+  userGuess.setActionCommand("myTF");
+
+  guess = new JButton("Guess");
+  again = new JButton("Play Again");
+
+  userGuess.addActionListener(this);
+  guess.addActionListener(this);
+  again.addActionListener(this);
+
+  enter = new JLabel("Enter your guess:");
+  highLow = new JLabel("");
+  last = new JLabel("");
+
+  frame.add(enter);
+  frame.add(userGuess);
+  frame.add(guess);
+  frame.add(highLow);
+  frame.add(last);
+  frame.add(again);
+
+  frame.setVisible(true);
+
+}
+
+public void actionPerformed(ActionEvent ae){
+  if(ae.getActionCommand().equals("Guess")){
+    Random r = new Random();
+    int random = r.nextInt(100) + 1;
+    int input = Integer.parseInt(userGuess.getText());
+  
+  if(userGuess == random){
+    highLow.setText("You got it!");
+    last.setText("Last guess was: " + random);
+  }
+  else if(userGuess < random){
+    highLow.setText("Too low!");
+    last.setText("Last guess was: " + userGuess);
+  }
+  else{
+    highLow.setText("Too high!");
+    last.setText("Last guess was: " + userGuess);
+  }}
+
+  if(ae.getActionCommand().equals("Play Again")){
+    Random r = new Random();
+    int random = r.nextInt(100) + 1;
+    enter.setText("Enter your guess:");
+    highLow.setText("");
+    last.setText("");
+    userGuess.setText("");
+  }
+  else{
+    highLow.setText("You pressed Enter. Please press the Guess button.");
+  }
+
+}
+
+}
